7766433a9712725d9e8e9f165178d287c320df79
Luiz Costa <luiz@thenets.org>
2019-09-06 23:15:17 -0300

    Added support to YAML config file

--- app.go
+++ app.go
@@ -2,9 +2,36 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
+	"log"
+
 	"gopkg.in/yaml.v2"
 )
 
+// ConfigData configs from YAML file
+type ConfigData struct {
+    Name string `yaml:"name"`
+    CMD []string `yaml:"cmd"`
+	CPU  struct {
+		Limit int `yaml:"limit"`
+	}
+	Linux struct {
+		User string `yaml:"user"`
+	}
+}
+
 func main() {
-	fmt.Println("oi")
+	myConfigData, err := ioutil.ReadFile("./samples/sleep.yaml")
+	if err != nil {
+        log.Fatalf("error: %v", err)
+    }
+	fmt.Print(string(myConfigData))
+
+	t := ConfigData{}
+
+	err = yaml.Unmarshal([]byte(myConfigData), &t)
+	if err != nil {
+		log.Fatalf("error: %v", err)
+	}
+	fmt.Printf("--- t:\n%v\n\n", t)
 }
